e8bd327bfcc591c6d5b6f870fd78c76ee17b421f
amart26 <amartj0713@gmail.com>
2025-01-10 14:38:57 -0500

    dark-mode

--- public/js/darkMode.js
+++ public/js/darkMode.js
@@ -1,15 +1,27 @@
 document.getElementById("toggle-dark-mode").addEventListener("click", toggleDarkMode); 
 
+//Variables
 const bodyStyle = document.getElementById('body');
 const leftContainer = document.getElementById('left-container'); 
 const rightContainer = document.getElementById('right-container'); 
 const footerContainer = document.getElementById('footer-container'); 
 
 
-function toggleDarkMode() {
+let isDarkModeEnabled = false; 
+
  //When clicked toggle dark mode
-    bodyStyle.classList.add("dark-mode");
-    leftContainer.classList.add("dark-mode");
-    rightContainer.classList.add("dark-mode");
-    footerContainer.classList.add("dark-mode");
+function toggleDarkMode() {
+    bodyStyle.classList.toggle("dark-mode");
+    leftContainer.classList.toggle("dark-mode");
+    rightContainer.classList.toggle("dark-mode");
+    footerContainer.classList.toggle("dark-mode");
+}
+
+function changeImg(){
+   if(isDarkModeEnabled == true){
+      document.getElementById("toggle-dark-mode").src = "/public/images/light-mode.png";
+   }
+   else if(isDarkModeEnabled == false){
+      document.getElementById("toggle-dark-mode").src="/public/images/dark-mode.png";
+   }
 }
