ea74b8238b88a9434013c306f203fdee45a2f660
amart26 <amartj0713@gmail.com>
2025-01-11 20:40:05 -0500

    fixed-images

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.get('/audio-files', (req, res) => {
 // Endpoint to serve config.html
 app.get('/config', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'pages', 'config.html'));
-});
+}); 
 
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
