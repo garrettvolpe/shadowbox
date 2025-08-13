ba5e36122eeb780891c104232d8cc64ca04d5841
Anthony <58822386+amart26@users.noreply.github.com>
2025-08-13 15:02:56 -0400

    Add StateManager to timer

--- public/js/Timer.js
+++ public/js/Timer.js
@@ -1,83 +1,114 @@
-const timerDisplay = document.getElementById("timer-display");
-const timerLabel = document.getElementById("timer-label");
+const timerDisplay = document.getElementById('timer-display');
+const timerLabel = document.getElementById('timer-label');
 
-const startButton = document.getElementById("start-button");
-const stopButton = document.getElementById("stop-button");
-const pauseButton = document.getElementById("pause-button");
-const resumeButton = document.getElementById("resume-button");
-const resetButton = document.getElementById("reset-button");
+const startButton = document.getElementById('start-button');
+const stopButton = document.getElementById('stop-button');
+const pauseButton = document.getElementById('pause-button');
+const resumeButton = document.getElementById('resume-button');
+const resetButton = document.getElementById('reset-button');
 
-//ENUM - JS doesnt have default enumerators //
+// ENUM - JS doesnt have default enumerators //
+const StateManager = Object.freeze(
+    {INITIALSTATE: 0, WORKRUNNING: 1, RESTRUNNING: 2, RESETTIMER: 3});
 
 const TimerLabelText = Object.freeze({
-  IDLETXT: "ARE YOU READY?",
-  RUNNINGTXT: "WORK",
-  RESTTXT: "REST",
+  IDLETXT: 'ARE YOU READY?',
+  RUNNINGTXT: 'WORK',
+  RESTTXT: 'REST',
 });
 
 const ComboAudio = [
-  new Audio("./audio/basics/basic1.mp3"),
-  new Audio("./audio/basics/basic3.mp3"),
-  new Audio("./audio/basics/basic4.mp3"),
+  new Audio('./audio/basics/basic1.mp3'),
+  new Audio('./audio/basics/basic3.mp3'),
+  new Audio('./audio/basics/basic4.mp3'),
 ];
 
-const roundStartSound = new Audio("./audio/misc/bell_start.mp3");
-const roundEndSound = new Audio("./audio/misc/bell_end.mp3");
+const roundStartSound = new Audio('./audio/misc/bell_start.mp3');
+const roundEndSound = new Audio('./audio/misc/bell_end.mp3');
 
 let timerInterval;
 let comboInterval;
 
 /*                      ////////// Timer Class Formatting //////////
         -- Any variable with m_[VariableName] is a member of the class and
-        cannot be used outside the scope of said class. 
+        cannot be used outside the scope of said class.
 */
 class Timer {
   m_RoundCounter = 1;
-  m_IsWorkTimeRunning;
 
-  constructor(roundDuration, roundRestTime, numberOfRounds) {
+  constructor(roundDuration, roundRestTime, numberOfRounds, currentState) {
     this.m_RoundDuration = roundDuration;
     this.m_RoundRestTime = roundRestTime;
     this.m_NumberOfRounds = numberOfRounds;
+    this.m_CurrentState = currentState;
   }
 
+  Tick() {
+    // WORK TIMER RUNNING STATE
+    switch (this.m_CurrentState) {
+      case StateManager.INITIALSTATE:
+        this.InitializeTimer();
+        break;
+
+      case StateManager.WORKRUNNING:
+        this.m_RemainingTime--;
+        timerDisplay.textContent = this.FormatTimerText(this.m_RemainingTime);
+        if (this.m_RemainingTime <= 0) {
+          this.PlayAudio(roundEndSound);
+        }
+        if (this.m_RemainingTime <= 0) {
+          this.m_CurrentState = StateManager.RESTRUNNING;
+        }
+        break;
+
+      case StateManager.RESTRUNNING:
+        this.m_RemainingRestTime--;
+        timerLabel.textContent = TimerLabelText.RESTTXT;
+        timerDisplay.textContent =
+            this.FormatTimerText(this.m_RemainingRestTime);
+        if (this.m_RemainingRestTime <= 0 &&
+            this.m_RoundCounter < this.m_NumberOfRounds) {
+          this.m_CurrentState = StateManager.RESETTIMER;
+        }
+        if (this.m_RemainingRestTime == 0 &&
+            this.m_RoundCounter == this.m_NumberOfRounds) {
+          this.m_CurrentState = StateManager.INITIALSTATE;
+          
+          
+        }
+        break;
+      case StateManager.RESETTIMER:
+        this.m_RoundCounter++;
+        this.m_RemainingRestTime = this.m_RoundRestTime;
+        this.m_RemainingTime = this.m_RoundDuration;
+        this.m_CurrentState = StateManager.WORKRUNNING;
+        this.PlayAudio(roundStartSound);
+        break;
+    }
+  }
+
+
+
   InitializeTimer() {
+    clearInterval(timerInterval);
     this.m_RoundCounter = 1;
     this.m_RemainingTime = this.m_RoundDuration;
     this.m_RemainingRestTime = this.m_RoundRestTime;
-    this.m_IsWorkTimeRunning = false;
     timerDisplay.textContent = this.FormatTimerText(this.m_RemainingTime);
     timerLabel.textContent = TimerLabelText.IDLETXT;
+    
   }
 
   StartTimer() {
-    startButton.classList.add("hidden");
-    resumeButton.classList.remove("hidden");
+    this.m_CurrentState = StateManager.WORKRUNNING;
     this.PlayAudio(roundStartSound);
-    this.m_IsWorkTimeRunning = true;
-    timerInterval = setInterval(() => {
-      this.UpdateTimer();
-    }, 1000);
+    timerInterval = setInterval(() => this.Tick(), 1000);
   }
+  PauseTimer() {}
 
-  PauseTimer() {
-    clearInterval(comboInterval);
-    clearTimeout(timerInterval);
-  }
+  ResumeTimer() {}
 
-  ResumeTimer() {
-    timerInterval = setInterval(() => {
-      this.UpdateTimer();
-    }, 1000);
-  }
-
-  ResetTimer() {
-    startButton.classList.remove("hidden");
-    resumeButton.classList.add("hidden");
-    clearInterval(timerInterval);
-    clearInterval(comboInterval);
-    this.InitializeTimer();
-  }
+  ResetTimer() {}
 
   FormatTimerText(remainingTime) {
     let minutes;
@@ -86,43 +117,10 @@ class Timer {
     minutes = Math.floor(remainingTime / 60);
     seconds = remainingTime % 60;
 
-    let formattedMinutes = String(minutes).padStart(2, "0");
-    let formattedSeconds = String(seconds).padStart(2, "0");
+    let formattedMinutes = String(minutes).padStart(2, '0');
+    let formattedSeconds = String(seconds).padStart(2, '0');
 
     return `${formattedMinutes}:${formattedSeconds}`;
-  }
-
-  UpdateTimer() {
-    if (!this.m_IsWorkTimeRunning) {
-      clearInterval(comboInterval);
-    }
-
-    if (this.m_RemainingTime == 1) {
-      this.PlayAudio(roundEndSound);
-    }
-
-    if (this.m_RemainingTime >= 0) {
-      this.m_IsWorkTimeRunning = true;
-
-      timerLabel.textContent = TimerLabelText.RUNNINGTXT;
-      timerDisplay.textContent = this.FormatTimerText(this.m_RemainingTime);
-      this.m_RemainingTime--;
-    } else if (this.m_RemainingRestTime >= 0) {
-      this.m_IsWorkTimeRunning = false;
-      timerLabel.textContent = TimerLabelText.RESTTXT;
-      timerDisplay.textContent = this.FormatTimerText(this.m_RemainingRestTime);
-      this.m_RemainingRestTime--;
-    } else if (
-      this.m_RemainingRestTime <= 0 &&
-      this.m_RoundCounter != this.m_NumberOfRounds
-    ) {
-      this.InitializeNextRoundTimer();
-      this.PlayAudio(roundStartSound);
-    }
-    else 
-    {
-        this.ResetTimer();
-    }
   }
 
   PlayAudio(audioSource, ...index) {
@@ -132,21 +130,16 @@ class Timer {
       audioSource.play();
     }
   }
-
-  InitializeNextRoundTimer() {
-    this.m_RoundCounter++;
-    this.m_RemainingTime = this.m_RoundDuration;
-    this.m_RemainingRestTime = this.m_RoundRestTime;
-    timerDisplay.textContent = this.FormatTimerText(this.m_RemainingTime);
-    timerLabel.textContent = TimerLabelText.RUNNINGTXT;
-    this.m_IsWorkTimeRunning = true;
-  }
 }
 
-const newTimer = new Timer(10, 5, 2);
+const newTimer = new Timer(2, 2, 1, StateManager.INITIALSTATE);
 newTimer.InitializeTimer();
+console.log('# of Rounds: ' + newTimer.m_NumberOfRounds);
+setInterval(() => {
+  console.log(newTimer.m_CurrentState);
+}, 5000);
 
-startButton.addEventListener("click", () => newTimer.StartTimer());
-pauseButton.addEventListener("click", () => newTimer.PauseTimer());
-resumeButton.addEventListener("click", () => newTimer.ResumeTimer());
-resetButton.addEventListener("click", () => newTimer.ResetTimer());
+startButton.addEventListener('click', () => newTimer.StartTimer());
+pauseButton.addEventListener('click', () => newTimer.PauseTimer());
+resumeButton.addEventListener('click', () => newTimer.ResumeTimer());
+resetButton.addEventListener('click', () => newTimer.ResetTimer());
