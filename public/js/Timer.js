d79d4ff2da2f4df4fd45d32e951624179181065c
amart26 <58822386+amart26@users.noreply.github.com>
2025-08-13 05:56:44 -0400

    Add functioning start, resume, pause, reset buttons

--- public/js/Timer.js
+++ public/js/Timer.js
@@ -1,8 +1,18 @@
+
+const timerDisplay = document.getElementById('timer-display');
+const startButton = document.getElementById('start-button');
+const stopButton = document.getElementById('stop-button');
+const pauseButton = document.getElementById('pause-button');
+const resumeButton = document.getElementById('resume-button');
+const resetButton = document.getElementById('reset-button');
+
+//ENUM - JS doesnt have default enumerators //
 const TimerState = Object.freeze({
-    RUNNING: true,
+    RUNNING: true,  
     PAUSED: true
 });
 
+let timer; 
 
 class Timer {
     constructor(roundDuration, roundRestTime, numberOfRounds){
@@ -11,20 +21,65 @@ class Timer {
         this.m_NumberOfRounds = numberOfRounds;
     }
 
+    InitializeTimer()
+    {
+        this.m_RemainingTime = this.m_RoundDuration;
+        timerDisplay.textContent = this.m_RoundDuration;
+    }
+
     StartTimer()
     {
-
+        timer = setInterval(() => {
+           this.UpdateTimer();
+        }, 1000);    
     }
 
     PauseTimer()
     {
-
+        clearTimeout(timer);
     }
 
     ResumeTimer()
     {
-
+        timer = setInterval(() => {
+            this.UpdateTimer();
+        }, 1000);
     }
 
+    ResetTimer()
+    {
+        if(this.m_RemainingTime == 0)
+        {
+            this.m_RemainingTime = this.m_RoundDuration;
+            timerDisplay.textContent = this.m_RemainingTime;
+        }
+        
+    }
 
+    FormatTimerText()
+    {
+        let minutes; 
+        let seconds; 
+
+        minutes /= this.m_RoundDuration; 
+        seconds %= this.m_RoundDuration / minutes;
+       
+    }
+
+    UpdateTimer()
+    {
+         if(this.m_RemainingTime > 0)
+            {
+                this.m_RemainingTime--; 
+                timerDisplay.textContent = this.m_RemainingTime; 
+            }
+    }
 }
+
+const newTimer = new Timer(10, 60, 3);
+newTimer.InitializeTimer();
+
+startButton.addEventListener("click", () => newTimer.StartTimer());
+pauseButton.addEventListener("click", ()=> newTimer.PauseTimer());
+resumeButton.addEventListener("click", () => newTimer.ResumeTimer());
+resetButton.addEventListener("click", ()=> newTimer.ResetTimer()); 
