9273cebed2a4e8a2225033558d373fb866dead61
amart26 <58822386+amart26@users.noreply.github.com>
2025-08-13 04:54:11 -0400

    Add Timer class

---
+++ public/js/Timer.js
@@ -0,0 +1,30 @@
+const TimerState = Object.freeze({
+    RUNNING: true,
+    PAUSED: true
+});
+
+
+class Timer {
+    constructor(roundDuration, roundRestTime, numberOfRounds){
+        this.m_RoundDuration = roundDuration; 
+        this.m_RoundRestTime = roundRestTime; 
+        this.m_NumberOfRounds = numberOfRounds;
+    }
+
+    StartTimer()
+    {
+
+    }
+
+    PauseTimer()
+    {
+
+    }
+
+    ResumeTimer()
+    {
+
+    }
+
+
+}
